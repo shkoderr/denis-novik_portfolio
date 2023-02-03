bb92232023c6051ea113c38f0cf9a54ea35bf525
Timur <elecktrohachyck@gmail.com>
2023-02-03 14:55:28 +0400

    made a lng switch button

--- assets/index.js
+++ assets/index.js
@@ -9,3 +9,28 @@ document.addEventListener('click', (event) => {
     toggleButton.classList.remove('active')
   }
 })
+
+const russianBtn = document.getElementById('russianBtn')
+const englishBtn = document.getElementById('englishBtn')
+const textEnglish = document.getElementById('#textEnglish')
+const textRussian = document.getElementById('#textRussian')
+let currentLang = 'English'
+
+
+englishBtn.addEventListener('click', () => {
+  currentLang = 'English'
+  englishBtn.style.color = '#000'
+  russianBtn.style.color = '#828282'
+  textEnglish.style.display = "flex"
+  textRussian.style.display = "none"
+})
+
+russianBtn.addEventListener('click', () => {
+  currentLang = 'Russian'
+  russianBtn.style.color = '#000'
+  englishBtn.style.color = '#828282'
+  textRussian.style.display = "flex"
+  textEnglish.style.display = "none"
+})
+
+englishBtn.click()
