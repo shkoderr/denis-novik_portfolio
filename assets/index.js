b09395d564d8c03d1076f8325e03c1fbfbac781e
Timur <elecktrohachyck@gmail.com>
2023-02-07 12:20:38 +0400

    worked with navbar

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 const toggleButton = document.querySelector('.nav-toggle');
+const nav = getElementById('#nav')
+const link = getElementById('#nav__link')
 
 toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('active');
+  nav.classList.toggle('active');
+  link.classList.toggle('active');
 });
 
 document.addEventListener('click', (event) => {
@@ -12,8 +16,6 @@ document.addEventListener('click', (event) => {
 
 const russianBtn = document.getElementById('russianBtn')
 const englishBtn = document.getElementById('englishBtn')
-const textEnglish = document.getElementById('#textEnglish')
-const textRussian = document.getElementById('#textRussian')
 let currentLang = 'English'
 
 
@@ -21,16 +23,12 @@ englishBtn.addEventListener('click', () => {
   currentLang = 'English'
   englishBtn.style.color = '#000'
   russianBtn.style.color = '#828282'
-  textEnglish.style.display = "flex"
-  textRussian.style.display = "none"
 })
 
 russianBtn.addEventListener('click', () => {
   currentLang = 'Russian'
   russianBtn.style.color = '#000'
   englishBtn.style.color = '#828282'
-  textRussian.style.display = "flex"
-  textEnglish.style.display = "none"
 })
 
 englishBtn.click()
